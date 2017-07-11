2db946b6fe7cd4e7880300da79b2c1c91197670c
shajuk <shaju.shajuk@gmail.com>
2017-07-11 08:31:23 +0530

    my pizza store changes

---
+++ src/com/hotel/dao/PizzaStoreDAO.java
@@ -0,0 +1,53 @@
+package com.hotel.dao;
+
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.hotel.pojo.Customer;
+import com.hotel.util.HibernateUtil;
+
+public class PizzaStoreDAO {
+	
+	public void findTotalDeliveryMadeByEachSalesBoy(){
+		Session session=null;
+		try{
+			session=HibernateUtil.getSessionFactory().openSession();
+			Transaction t=session.beginTransaction();
+			Query q=session.createQuery(" select pizzaOrder.salesBoys.salesBoyId, count(pizzaOrder.salesBoys.salesBoyId) "
+					+ "from PizzaOrder pizzaOrder group by pizzaOrder.salesBoys.salesBoyId");
+			@SuppressWarnings("unchecked")
+			Iterator<Object[]> it=q.iterate();
+			while(it.hasNext()){
+				Object[] row=(Object[]) it.next();
+				System.out.println("SalesBoy Id: "+row[0]);
+				System.out.println("Total Delivery Made: "+row[1]);
+			}
+			t.commit();
+		}catch(HibernateException e){
+			e.printStackTrace();
+		}finally{
+			session.close();
+		}
+	}
+	
+	public List<Customer> findCustomerWhoPlacedLargestNoOfOrders(){
+		Session session=null;
+		try{
+			session=HibernateUtil.getSessionFactory().openSession();
+			Transaction t=session.beginTransaction();
+			
+			t.commit();
+		}catch(HibernateException e){
+			e.printStackTrace();
+		}finally{
+			session.close();
+		}
+		return null;
+	}
+}
--- src/com/hotel/main/Main.java
+++ src/com/hotel/main/Main.java
@@ -7,6 +7,8 @@ import java.util.Set;
 import com.hotel.dao.CustomerDAO;
 import com.hotel.dao.OrderDAO;
 import com.hotel.dao.PizzaDAO;
+import com.hotel.dao.PizzaStoreDAO;
+import com.hotel.dao.SalesBoyDAO;
 import com.hotel.pojo.Address;
 import com.hotel.pojo.Customer;
 import com.hotel.pojo.MembershipCard;
@@ -32,6 +34,36 @@ public class Main {
 //		customerDAO.storeCustomerDetails(customer, address);
 //		MembershipCard card=new MembershipCard();
 //		customerDAO.enrollForMembershipCard(customer.getCustomerNo(), card);
+//		
+//		Customer customer2=new Customer();
+//		Address address2=new Address();
+//		customer2.setCustomerName("Linto");
+//		address2.setCity("Kochi");
+//		address2.setContactNo(78569);
+//		address2.setDoorNo("875");
+//		address2.setEmailId("linto@gmail.com");
+//		address2.setPincode(87458);
+//		address2.setState("Kerala");
+//		address2.setStreet("jango street");
+//		customer2.setAddress(address2);
+//		customerDAO.storeCustomerDetails(customer2, address2);
+//		MembershipCard card2=new MembershipCard();
+//		customerDAO.enrollForMembershipCard(customer2.getCustomerNo(), card2);
+//		
+//		Customer customer3=new Customer();
+//		Address address3=new Address();
+//		customer3.setCustomerName("Rinku");
+//		address3.setCity("Delhi");
+//		address3.setContactNo(24567);
+//		address3.setDoorNo("745");
+//		address3.setEmailId("rinku@gmail.com");
+//		address3.setPincode(623541);
+//		address3.setState("Delhi");
+//		address3.setStreet("L.B street");
+//		customer3.setAddress(address3);
+//		customerDAO.storeCustomerDetails(customer3, address3);
+//		MembershipCard card3=new MembershipCard();
+//		customerDAO.enrollForMembershipCard(customer3.getCustomerNo(), card3);
 		//Scenario #1 ends
 		
 		//Scenario #2 
@@ -54,22 +86,61 @@ public class Main {
 //		pizzas.add(p5);
 //		pizzaDAO.createPizzas(pizzas);
 		
-		OrderDAO orderDAO=new OrderDAO();
+//		OrderDAO orderDAO=new OrderDAO();
 //		Set<PizzaOrder> orderObjs=new HashSet<PizzaOrder>();
 //		PizzaOrder o1=new PizzaOrder();
 //		o1.setOrderDate(new Date());
 //		orderObjs.add(o1);
-//		
 //		PizzaOrder o2=new PizzaOrder();
 //		o2.setOrderDate(new Date());
 //		orderObjs.add(o2);
+//		PizzaOrder o3=new PizzaOrder();
+//		o3.setOrderDate(new Date());
+//		orderObjs.add(o3);
+//		orderDAO.createOrder(18, orderObjs);
 //		
-//		orderDAO.createOrder(2, orderObjs);
-//		System.out.println(o1.getOrderId());
-//		System.out.println(o2.getOrderId());
+//		Set<PizzaOrder> orderObjs1=new HashSet<PizzaOrder>();
+//		PizzaOrder oc1=new PizzaOrder();
+//		oc1.setOrderDate(new Date());
+//		orderObjs1.add(oc1);
+//		orderDAO.createOrder(17, orderObjs1);
+//
+//		Set<PizzaOrder> orderObjs2=new HashSet<PizzaOrder>();
+//		PizzaOrder oc2=new PizzaOrder();
+//		oc2.setOrderDate(new Date());
+//		orderObjs2.add(oc2);
+//		PizzaOrder oc3=new PizzaOrder();
+//		oc3.setOrderDate(new Date());
+//		orderObjs2.add(oc3);
+//		orderDAO.createOrder(19, orderObjs2);
 		
-		orderDAO.createOrderDetail(18, 2, 1, 60);
+//		orderDAO.createOrderDetail(1, 2, 1, 80);
+//		orderDAO.createOrderDetail(2, 5, 1, 50);
+//		orderDAO.createOrderDetail(3, 1, 2, 150);
+//		
+//		orderDAO.createOrderDetail(4, 5, 1, 80);
+//		
+//		orderDAO.createOrderDetail(5, 3, 1, 100);
+//		orderDAO.createOrderDetail(6, 4, 2, 120);
+//		
 		//Scenario #2 ends
+		
+		//Scenario #3
+//		SalesBoyDAO sbd=new SalesBoyDAO();
+//		sbd.assignOrderToSalesBoy(1, 100);
+//		sbd.assignOrderToSalesBoy(2, 100);
+//		sbd.assignOrderToSalesBoy(3, 100);
+//		
+//		sbd.assignOrderToSalesBoy(4, 101);
+//		
+//		sbd.assignOrderToSalesBoy(5, 102);
+//		sbd.assignOrderToSalesBoy(6, 102);
+		//Scenario #3 ends
+		
+		//Scenario #4
+		PizzaStoreDAO pizzaStoreDAO=new PizzaStoreDAO();
+		pizzaStoreDAO.findTotalDeliveryMadeByEachSalesBoy();
+		//Scenario #4 ends
 		
 		
 	}
